4d4a32a1185d4a95c67b19b6e8a3426e3429125c
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-05-30 16:41:39 +0100

    Add error functionality

--- src/context/context.js
+++ src/context/context.js
@@ -19,6 +19,9 @@ const GithubProvider = ({ children }) => {
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
+
   // check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -28,17 +31,23 @@ const GithubProvider = ({ children }) => {
         } = data;
         setRequests(remaining);
         if (remaining === 0) {
-          // throw an error
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
         }
       })
       .catch((error) => console.log(err));
   };
 
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
   // error
   useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
