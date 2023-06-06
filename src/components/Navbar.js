4606b835ccc8b2f73455e940daa0bb43bca767c4
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-06-06 11:18:52 +0100

    Fix toUpperCase func

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
       {isUser && user.picture && <img src={user.picture} alt={user.name} />}
       {isUser && user.name && (
         <h4>
-          Welcome, <strong>{user.name.toUppercase()}</strong>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
         </h4>
       )}
       {isUser ? (
